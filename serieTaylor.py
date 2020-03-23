2082cfb46e8d1f2b3810d53174563329c4e47ee1
martinsspn <47855595+martinsspn@users.noreply.github.com>
2020-03-23 17:12:42 -0300

    Delete serieTaylor.py

--- serieTaylor.py
+++
@@ -1,8 +0,0 @@
-import math
-x = 0
-for k in range(10):
-    if k%2 != 0:
-        y = (-1)**((k-1)/2) * k* math.cos(x) + (-1)**((k+1)/2) * x * math.sin(x)
-    else:
-        y = (-1)**(k/2) * k * math.sin(x) + (-1)**(k/2) * x * math.cos(x)
-    print y

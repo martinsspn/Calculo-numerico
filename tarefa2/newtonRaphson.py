05d7993d8e9db3834bd42fff073db076067b99e5
martinsspn <martinsspn@github.com>
2020-09-17 11:30:04 -0300

    adição método de newthon

---
+++ tarefa2/newtonRaphson.py
@@ -0,0 +1,9 @@
+def newthonRaphson(x):
+    for n in range (0,100):
+        x = x - ((x**3 - 1.7*(x**2) - 12.78*x - 10.08)/(3*x**2 - 3.4*x - 12.78))
+    return x
+
+arq = open("ptsNewtonRaphson.pts", "w")
+arq.write(str(newthonRaphson(-3)) + " 0\n")
+arq.write(str(newthonRaphson(-1)) + " 0\n")
+arq.write(str(newthonRaphson(4)) + " 0 \n")
